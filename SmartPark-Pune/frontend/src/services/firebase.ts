1bb5ce86679ea8ca8614ea553204cab4bfc2d06c
Sandeshbhandare26 <bhandaresandesh26@gmail.com>
2026-04-03 19:35:58 +0530

    ml model og

--- SmartPark-Pune/frontend/src/services/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-};
-
-const app = initializeApp(firebaseConfig);
-export default app;
